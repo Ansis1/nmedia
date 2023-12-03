4137e4cc7c138e29231a1576fff2a69fc5e4df37
Ansis Kalnyn <ansis.kalnyn@yandex.ru>
2023-12-03 22:44:31 +0300

    add code and lements for 03_constraint_layout homework

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -18,12 +18,14 @@ class MainActivity : AppCompatActivity() {
 
         tvTitlePost.setText("Очень длинный заголовок, много - много слов и букв. Совсем.")
         tvDatePost.setText("01 мая в 18:00")
-        tvTextPost.setText("AndroidX libraries are moving to a default minimum supported Android API " +
-                "level 19 (previously 14) starting with releases in October, 2023. According to Play " +
-                "Store check-in data, nearly all Android users have devices on API 19 or newer, so it" +
-                "’s no longer necessary to support legacy versions. This change will help AndroidX " +
-                "libraries maximize the potential number of users for app developers and aligns with " +
-                "Google Play Services and Android NDK.")
+        tvTextPost.setText(
+            "AndroidX libraries are moving to a default minimum supported Android API " +
+                    "level 19 (previously 14) starting with releases in October, 2023. According to Play " +
+                    "Store check-in data, nearly all Android users have devices on API 19 or newer, so it" +
+                    "’s no longer necessary to support legacy versions. This change will help AndroidX " +
+                    "libraries maximize the potential number of users for app developers and aligns with " +
+                    "Google Play Services and Android NDK."
+        )
         tvLikesCnt.setText("123")
         tvLookCnt.setText("1K")
         tvShareCnt.setText("4K")
