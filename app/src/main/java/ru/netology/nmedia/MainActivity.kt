54d48d2fa5a4c14a9b1774620b162df1a00d00d8
Ansis Kalnyn <ansis.kalnyn@yandex.ru>
2023-12-04 23:25:39 +0300

    add code and elements for 04_events homework

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,33 +2,107 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.ImageButton
 import android.widget.TextView
+import androidx.appcompat.content.res.AppCompatResources
 
 class MainActivity : AppCompatActivity() {
+
+    val counterMap = mutableMapOf<String, Long>()
+    val counterStringsMap = mutableMapOf(Pair("looked", "1K"))
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val tvTitlePost: TextView = findViewById(R.id.tv_titlepost)
         val tvDatePost: TextView = findViewById(R.id.tv_datepost)
         val tvTextPost: TextView = findViewById(R.id.tv_textpost)
         val tvLikesCnt: TextView = findViewById(R.id.tv_likes_cnt)
         val tvLookCnt: TextView = findViewById(R.id.tv_look_cnt)
         val tvShareCnt: TextView = findViewById(R.id.tv_share_cnt)
+        val lkeIb: ImageButton = findViewById(R.id.ib_liked)
+        val shareIb: ImageButton = findViewById(R.id.ib_shared)
+        tvLikesCnt.setText(counterStringsMap.get("liked") ?: "0")
+        tvShareCnt.setText(counterStringsMap.get("shared") ?: "0")
+        tvLookCnt.setText(counterStringsMap.get("looked") ?: "0")
 
-        tvTitlePost.setText("Очень длинный заголовок, много - много слов и букв. Совсем.")
-        tvDatePost.setText("01 мая в 18:00")
-        tvTextPost.setText(
-            "AndroidX libraries are moving to a default minimum supported Android API " +
-                    "level 19 (previously 14) starting with releases in October, 2023. According to Play " +
-                    "Store check-in data, nearly all Android users have devices on API 19 or newer, so it" +
-                    "’s no longer necessary to support legacy versions. This change will help AndroidX " +
-                    "libraries maximize the potential number of users for app developers and aligns with " +
-                    "Google Play Services and Android NDK."
-        )
-        tvLikesCnt.setText("123")
-        tvLookCnt.setText("1K")
-        tvShareCnt.setText("4K")
+        shareIb.setOnClickListener {
+
+            reloadCntCounters("shared", 10, tvShareCnt);
+        }
+
+        var isLiked = false
+        lkeIb.setOnClickListener {
+
+            if (!isLiked) {
+                isLiked = true
+                lkeIb.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.liked))
+                reloadCntCounters("liked", tvForChange = tvLikesCnt);
+            } else {
+                if ((counterMap.get("liked") ?: 0) > 0) {
+                    reloadCntCounters("liked", -1, tvLikesCnt);
+                }
+                lkeIb.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.like))
+                isLiked = false
+            }
+
+        }
+        tvTitlePost.setText(getString(R.string.post_title))
+        tvDatePost.setText(getString(R.string.post_date))
+        tvTextPost.setText(getString(R.string.post_txt))
+
+    }
+
+    private fun reloadCntCounters(type: String, summ: Long = 1, tvForChange: TextView) {
+
+        val prevCnt = counterMap.get(type) ?: 0
+        val currCnt = prevCnt + summ
+        counterMap.put(type, currCnt)
+        val stringNumb = currCnt.toString()
+
+        when {
+
+            currCnt > 999_000 -> {
+
+                val finishString = "${stringNumb[0]}${
+                    if (stringNumb[1] != '0') {
+                        "." + stringNumb[1]
+                    } else {
+                        ""
+                    }
+                }M"
+                counterStringsMap.put(type, finishString)
+                tvForChange.text = finishString
+            }
+
+            currCnt > 9_999 -> {
+                val finishString = "${stringNumb[0]}${stringNumb[1]}K"
+                counterStringsMap.put(type, finishString)
+                tvForChange.text = finishString
+
+            }
+
+            currCnt > 999 -> {
+
+                val finishString = "${stringNumb[0]}${
+                    if (stringNumb[1] != '0') {
+                        "." + stringNumb[1]
+                    } else {
+                        ""
+                    }
+                }K"
+                counterStringsMap.put(type, finishString)
+                tvForChange.text = finishString
+
+            }
+
+
+            else -> {
+                tvForChange.setText(stringNumb)
+            }
+
+        }
 
     }
 }
