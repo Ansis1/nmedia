5265671ed5c99882ca49a1cd84f0384656e1c331
Ansis K <ansis.kalnyn@yandex.ru>
2023-12-07 21:43:41 +0300

    add code for 05_mvvm Homework

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,66 +1,83 @@
 package ru.netology.nmedia
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
-import android.widget.TextView
-import androidx.appcompat.content.res.AppCompatResources
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import ru.netology.nmedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    val counterMap = mutableMapOf<String, Long>()
-    val counterStringsMap = mutableMapOf(Pair("looked", "1K"))
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val tvTitlePost: TextView = findViewById(R.id.tv_titlepost)
-        val tvDatePost: TextView = findViewById(R.id.tv_datepost)
-        val tvTextPost: TextView = findViewById(R.id.tv_textpost)
-        val tvLikesCnt: TextView = findViewById(R.id.tv_likes_cnt)
-        val tvLookCnt: TextView = findViewById(R.id.tv_look_cnt)
-        val tvShareCnt: TextView = findViewById(R.id.tv_share_cnt)
-        val lkeIb: ImageButton = findViewById(R.id.ib_liked)
-        val shareIb: ImageButton = findViewById(R.id.ib_shared)
-        tvLikesCnt.setText(counterStringsMap.get("liked") ?: "0")
-        tvShareCnt.setText(counterStringsMap.get("shared") ?: "0")
-        tvLookCnt.setText(counterStringsMap.get("looked") ?: "0")
-
-        shareIb.setOnClickListener {
-
-            reloadCntCounters("shared", 10, tvShareCnt);
-        }
-
-        var isLiked = false
-        lkeIb.setOnClickListener {
-
-            if (!isLiked) {
-                isLiked = true
-                lkeIb.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.liked))
-                reloadCntCounters("liked", tvForChange = tvLikesCnt);
-            } else {
-                if ((counterMap.get("liked") ?: 0) > 0) {
-                    reloadCntCounters("liked", -1, tvLikesCnt);
-                }
-                lkeIb.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.like))
-                isLiked = false
+        val viewModel: PostViewModel by viewModels()
+        viewModel.data.observe(this) { post ->
+            with(binding) {
+                tvTextpost.setText(post.content)
+                tvTitlepost.setText(post.title)
+                tvDatepost.setText(post.published.toString())
+                tvLikesCnt.setText(post.counterStringsMap.get("liked") ?: "0")
+                tvLookCnt.setText(post.counterStringsMap.get("looked") ?: "0")
+                tvShareCnt.setText(post.counterStringsMap.get("shared") ?: "0")
+                ibLiked.setImageResource(if (post.likedByMe) R.drawable.liked else R.drawable.like)
             }
 
         }
-        tvTitlePost.setText(getString(R.string.post_title))
-        tvDatePost.setText(getString(R.string.post_date))
-        tvTextPost.setText(getString(R.string.post_txt))
+
+        binding.ibShared.setOnClickListener {
+
+            viewModel.share()
+        }
+
+        binding.ibLiked.setOnClickListener {
+
+            viewModel.like()
+
+        }
 
     }
 
-    private fun reloadCntCounters(type: String, summ: Long = 1, tvForChange: TextView) {
 
-        val prevCnt = counterMap.get(type) ?: 0
-        val currCnt = prevCnt + summ
-        counterMap.put(type, currCnt)
+}
+
+class PostRepositoryImpl : PostRepository {
+
+    var nPost = Post(
+        1234,
+        "Ansis",
+        "This is content of Post.",
+        System.currentTimeMillis(),
+        false,
+        mutableMapOf(Pair("1", 0L)),
+        mutableMapOf(Pair("looked", "1K")),
+        "Test title"
+    )
+
+    private val data = MutableLiveData(nPost)
+    override fun get(): LiveData<Post> = data
+    override fun like() {
+        nPost = nPost.copy(likedByMe = !nPost.likedByMe) //Меняем зн. для смены иконки
+        val cnt = if (nPost.likedByMe) 1L else {
+            if ((nPost.counterMap.get("liked") ?: 0) > 0) -1L else 0
+        }
+        reloadCntCounters("liked", cnt)
+    }
+
+    override fun share() {
+        reloadCntCounters("shared", 10);
+    }
+
+    override fun reloadCntCounters(type: String, summ: Long) {
+        val currPost: Post = nPost.copy()
+        val prevCnt = currPost.counterMap.get(type) ?: 0
+        val currCnt = prevCnt + (if (summ == 0L) 1 else summ)
+        currPost.counterMap.put(type, currCnt)
         val stringNumb = currCnt.toString()
-
         when {
 
             currCnt > 999_000 -> {
@@ -72,14 +89,12 @@ class MainActivity : AppCompatActivity() {
                         ""
                     }
                 }M"
-                counterStringsMap.put(type, finishString)
-                tvForChange.text = finishString
+                currPost.counterStringsMap.put(type, finishString)
             }
 
             currCnt > 9_999 -> {
                 val finishString = "${stringNumb[0]}${stringNumb[1]}K"
-                counterStringsMap.put(type, finishString)
-                tvForChange.text = finishString
+                currPost.counterStringsMap.put(type, finishString)
 
             }
 
@@ -92,17 +107,38 @@ class MainActivity : AppCompatActivity() {
                         ""
                     }
                 }K"
-                counterStringsMap.put(type, finishString)
-                tvForChange.text = finishString
-
+                currPost.counterStringsMap.put(type, finishString)
             }
 
-
-            else -> {
-                tvForChange.setText(stringNumb)
-            }
-
+            else -> currPost.counterStringsMap.put(type, stringNumb)
         }
 
+        data.value = currPost
     }
+}
+
+interface PostRepository {
+    fun get(): LiveData<Post> // получить объект Post
+    fun like() //нажать понравилось
+    fun share() //поделиться
+    fun reloadCntCounters(type: String, summ: Long) //перезалить счетчики
+}
+
+data class Post(
+    val id: Long,
+    val author: String,
+    val content: String,
+    val published: Long,
+    val likedByMe: Boolean,
+    val counterMap: MutableMap<String, Long>,
+    val counterStringsMap: MutableMap<String, String>,
+    val title: String
+)
+
+class PostViewModel : ViewModel() {
+
+    private var repository: PostRepository = PostRepositoryImpl()
+    val data = repository.get()
+    fun like() = repository.like()
+    fun share() = repository.share()
 }
