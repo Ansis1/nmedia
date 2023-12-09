78cf5aa1bee5ffe0e1430f72512d7098d740f90a
Ansis Kalnyn <ansis.kalnyn@yandex.ru>
2023-12-09 14:37:13 +0300

    move data to Post class

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,10 +2,10 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.content.res.AppCompatResources
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,87 +18,90 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        with(binding) {
-        tvLikesCnt.setText(counterStringsMap.get("liked") ?: "0")
-        tvShareCnt.setText(counterStringsMap.get("shared") ?: "0")
-        tvLookCnt.setText(counterStringsMap.get("looked") ?: "0")
-    }
-        binding.ibShared.setOnClickListener {
+        var curPost = Post(
+            1234,
+            "Ansis",
+            "AndroidX libraries are moving to a default minimum supported Android API level " +
+                    "19 (previously 14) starting with releases in October, 2023. According to Play " +
+                    "Store check-in data, nearly all Android users have devices on API 19 or newer, " +
+                    "so it’s no longer necessary to support legacy versions. This change will help " +
+                    "AndroidX libraries maximize the potential number of users for app developers " +
+                    "and aligns with Google Play Services and Android NDK.",
+            "01 мая в 18:00",
+            false,
+            mutableMapOf(Pair("looked", 1_000)),
+            "Очень длинный заголовок, много - много слов и букв. Совсем."
 
-            reloadCntCounters("shared", 10,binding.tvShareCnt);
+        )
+        with(binding) {
+            tvLikesCnt.setText(getStringCounter(curPost.counterMap.get("liked") ?: 0))
+            tvShareCnt.setText(getStringCounter(curPost.counterMap.get("shared") ?: 0))
+            tvLookCnt.setText(getStringCounter(curPost.counterMap.get("looked") ?: 0))
+        }
+        binding.ibShared.setOnClickListener {
+            val currSharedCnt = curPost.counterMap.get("shared") ?: 0
+            curPost.counterMap.put("shared", 10 + currSharedCnt)
+            binding.tvShareCnt.setText(getStringCounter(currSharedCnt + 10))
         }
 
-        var isLiked = false
-        val lkeIb =  binding.ibLiked
+        val lkeIb = binding.ibLiked
         lkeIb.setOnClickListener {
 
-            if (!isLiked) {
-                isLiked = true
-                lkeIb.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.liked))
-                reloadCntCounters("liked", tvForChange = binding.tvLikesCnt);
-            } else {
-                if ((counterMap.get("liked") ?: 0) > 0) {
-                    reloadCntCounters("liked", -1, binding.tvLikesCnt);
-                }
-                lkeIb.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.like))
-                isLiked = false
+            curPost = curPost.copy(likedByMe = !curPost.likedByMe)
+            lkeIb.setImageResource(if (curPost.likedByMe) R.drawable.liked else R.drawable.like)
+            val currCntLike = curPost.counterMap.get("liked") ?: 0
+            val cnt = currCntLike + if (curPost.likedByMe) 1L else {
+                if (currCntLike > 0) -1L else 0
             }
-
+            curPost.counterMap.put("liked", cnt)
+            binding.tvLikesCnt.setText(getStringCounter(curPost.counterMap.get("liked") ?: 0))
         }
 
+
         with(binding) {
-            tvTitlepost.setText(getString(R.string.post_title))
-            tvDatepost.setText(getString(R.string.post_date))
-            tvTextpost.setText(getString(R.string.post_txt))
+            tvTitlepost.setText(curPost.title)
+            tvDatepost.setText(curPost.published)
+            tvTextpost.setText(curPost.content)
         }
     }
 
-    private fun reloadCntCounters(type: String, summ: Long = 1, tvForChange: TextView) {
+    private fun getStringCounter(currCnt: Long): String {
 
-        val prevCnt = counterMap.get(type) ?: 0
-        val currCnt = prevCnt + summ
-        counterMap.put(type, currCnt)
         val stringNumb = currCnt.toString()
 
-        when {
+        return when {
 
             currCnt > 9_99_999 -> {
 
-                val finishString = "${stringNumb[0]}${
+                "${stringNumb[0]}${
                     if (stringNumb[1] != '0') {
                         "." + stringNumb[1]
                     } else {
                         ""
                     }
                 }M"
-                counterStringsMap.put(type, finishString)
-                tvForChange.text = finishString
             }
 
             currCnt > 9_999 -> {
-                val finishString = "${stringNumb[0]}${stringNumb[1]}${stringNumb[2]}K"
-                counterStringsMap.put(type, finishString)
-                tvForChange.text = finishString
+                "${stringNumb[0]}${stringNumb[1]}${stringNumb[2]}K"
+
 
             }
 
             currCnt > 999 -> {
 
-                val finishString = "${stringNumb[0]}${
+                "${stringNumb[0]}${
                     if (stringNumb[1] != '0') {
                         "." + stringNumb[1]
                     } else {
                         ""
                     }
                 }K"
-                counterStringsMap.put(type, finishString)
-                tvForChange.text = finishString
 
             }
 
-
             else -> {
-                tvForChange.setText(stringNumb)
+                stringNumb
             }
 
         }
